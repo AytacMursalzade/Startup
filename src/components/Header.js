0ca8bfb7b82eb0c4c9ef547883c6f7f54e78f5c4
Aytac <aytacmurselzade381@gmail.com>
2024-07-01 23:56:46 +0400

    menufinish

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import NavbarItem from "./NavbarItem";
+import { GiHamburgerMenu, GiSplitCross } from "react-icons/gi";
 
+function Header({ bgColor }) {
+  const [isMobile, setIsMobile] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 600);
+    };
 
-function Header({bgColor}) {
+    window.addEventListener("resize", handleResize);
+    handleResize();
 
- 
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <>
-      <div style={{backgroundColor:bgColor}} className="headerNav h-[70px] flex justify-between " >
+      <div
+        style={{ backgroundColor: bgColor }}
+        className="headerNav h-[70px] flex justify-between"
+      >
         <NavbarItem />
-        <Link><div className="mr-[94px] text-white font-bold py-[20px] ">Sign in</div></Link>
-        
+        <Link>
+          <div className="mr-[94px] text-white font-bold py-[20px]">Sign in</div>
+        </Link>
       </div>
-      
       
     </>
   );
--- src/components/NavbarItem.js
+++ src/components/NavbarItem.js
@@ -1,331 +1,109 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { openSubmenu, closeSubmenu } from "../redux/navbarSlice";
-import { Link } from "react-router-dom";
-import Card from "./Card";
-import ShopCard from "./ShopCard";
-import BlogCard from "./BlogCard";
-import ShortcodeCard from "./ShortcodeCard";
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { openSubmenu, closeSubmenu } from '../redux/navbarSlice';
+import { Link } from 'react-router-dom';
 import { GiHamburgerMenu } from "react-icons/gi";
-import { FaTimes } from "react-icons/fa";
-import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import Card from './Card';
+import ShopCard from './ShopCard';
+import BlogCard from './BlogCard';
+import ShortcodeCard from './ShortcodeCard';
 
-const NavbarItem = (props) => {
+const Navbar = (props) => {
   const dispatch = useDispatch();
-  const { isSubmenuOpen } = useSelector((state) => state.navbar);
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const [expandedCategory, setExpandedCategory] = useState(null);
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
-
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
   const [hoveredItem, setHoveredItem] = useState(null);
 
-  const handleHover = (item) => {
-    dispatch(openSubmenu());
-    setHoveredItem(item);
-  };
-
-  const handleLeave = () => {
-    dispatch(closeSubmenu());
+  const closeAllMenus = () => {
     setHoveredItem(null);
   };
 
-  const toggleCategory = (category) => {
-    if (expandedCategory === category) {
-      setExpandedCategory(null);
-    } else {
-      setExpandedCategory(category);
-    }
+  const handleHover = (item) => {
+    dispatch(openSubmenu());
+    closeAllMenus();
+    setHoveredItem(item);
+    setIsSubmenuOpen(true);
   };
+
+  const handleLeave = () => {};
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (isSubmenuOpen && !event.target.closest('.menu-item')) {
+        setIsSubmenuOpen(false);
+        closeAllMenus();
+        dispatch(closeSubmenu());
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isSubmenuOpen, dispatch]);
+
+  const data = [
+    { id: 1, concept: "CATEGORIES", subMenu: <Card />, icon: <GiHamburgerMenu /> },
+    { id: 2, concept: "Home", subMenu: <Card /> },
+    { id: 3, concept: "Shop", subMenu: <ShopCard /> },
+    { id: 4, concept: "Vendors" },
+    { id: 5, concept: "Shortcodes", subMenu: <ShortcodeCard /> },
+    { id: 6, concept: "Blog", subMenu: <BlogCard /> },
+    { id: 7, concept: "Media" },
+    { id: 8, concept: "About" },
+    { id: 9, concept: "Contact" },
+    { id: 10, concept: "Pages" },
+  ];
 
   return (
     <div className={`h-[70px] bg-[${props.bgColor}] flex justify-between`}>
-      <nav className="w-full">
-        <ul
-          className={`flex gap-[44px] text-white items-center text-[14px] font-medium ${
-            isMobileMenuOpen ? "nav-links-mobile flex-col" : "nav-links"
-          }`}
-        >
-          <Link to="/categories">
-            <li
-              onMouseEnter={() => handleHover("categories")}
-              onMouseLeave={handleLeave}
-              className="relative ml-[70px] w-[240px] justify-between font-medium h-[70px] flex gap-[5px] items-center py-[20px] px-[15px] text-black bg-white"
-            >
-              <GiHamburgerMenu />
-              CATEGORIES
-              <span onClick={() => toggleCategory("categories")} className="cursor-pointer">
-                {expandedCategory === "categories" ? <FaChevronUp /> : <FaChevronDown />}
-              </span>
-              {isSubmenuOpen && hoveredItem === "categories" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <Card />
-                </div>
-              )}
-              {expandedCategory === "categories" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <ul>
-                    <Link to="/sub-category1">
-                      <li className="p-[10px]">Sub-category 1</li>
-                    </Link>
-                    <Link to="/sub-category2">
-                      <li className="p-[10px]">Sub-category 2</li>
-                    </Link>
-                  </ul>
-                </div>
-              )}
-            </li>
-          </Link>
-          <Link to="/home">
-            <li
-              onMouseEnter={() => handleHover("home")}
-              onMouseLeave={handleLeave}
-              className="relative"
-            >
-              Home
-              {isSubmenuOpen && hoveredItem === "home" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <Card />
-                </div>
-              )}
-            </li>
-          </Link>
-          <Link to="/shop">
-            <li
-              onMouseEnter={() => handleHover("shop")}
-              onMouseLeave={handleLeave}
-              className="relative"
-            >
-              Shop
-              <span onClick={() => toggleCategory("shop")} className="cursor-pointer">
-                {expandedCategory === "shop" ? <FaChevronUp /> : <FaChevronDown />}
-              </span>
-              {isSubmenuOpen && hoveredItem === "shop" && (
-                <div className="submenu absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <ShopCard />
-                </div>
-              )}
-              {expandedCategory === "shop" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <ul>
-                    <Link to="/sub-shop1">
-                      <li className="p-[10px]">Sub-shop 1</li>
-                    </Link>
-                    <Link to="/sub-shop2">
-                      <li className="p-[10px]">Sub-shop 2</li>
-                    </Link>
-                  </ul>
-                </div>
-              )}
-            </li>
-          </Link>
-          <Link to="/vendors">
-            <li className="relative">
-              Vendors
-              <span onClick={() => toggleCategory("vendors")} className="cursor-pointer">
-                {expandedCategory === "vendors" ? <FaChevronUp /> : <FaChevronDown />}
-              </span>
-              {expandedCategory === "vendors" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <ul>
-                    <Link to="/sub-vendor1">
-                      <li className="p-[10px]">Sub-vendor 1</li>
-                    </Link>
-                    <Link to="/sub-vendor2">
-                      <li className="p-[10px]">Sub-vendor 2</li>
-                    </Link>
-                  </ul>
-                </div>
-              )}
-            </li>
-          </Link>
-          <Link to="/shortcodes">
-            <li
-              onMouseEnter={() => handleHover("shortcodes")}
-              onMouseLeave={handleLeave}
-              className="relative"
-            >
-              Shortcodes
-              <span onClick={() => toggleCategory("shortcodes")} className="cursor-pointer">
-                {expandedCategory === "shortcodes" ? <FaChevronUp /> : <FaChevronDown />}
-              </span>
-              {isSubmenuOpen && hoveredItem === "shortcodes" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <ShortcodeCard />
-                </div>
-              )}
-              {expandedCategory === "shortcodes" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <ul>
-                    <Link to="/sub-shortcode1">
-                      <li className="p-[10px]">Sub-shortcode 1</li>
-                    </Link>
-                    <Link to="/sub-shortcode2">
-                      <li className="p-[10px]">Sub-shortcode 2</li>
-                    </Link>
-                  </ul>
-                </div>
-              )}
-            </li>
-          </Link>
-          <Link to="/blog">
-            <li
-              onMouseEnter={() => handleHover("blog")}
-              onMouseLeave={handleLeave}
-              className="relative"
-            >
-              Blog
-              <span onClick={() => toggleCategory("blog")} className="cursor-pointer">
-                {expandedCategory === "blog" ? <FaChevronUp /> : <FaChevronDown />}
-              </span>
-              {isSubmenuOpen && hoveredItem === "blog" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <BlogCard />
-                </div>
-              )}
-              {expandedCategory === "blog" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <ul>
-                    <Link to="/sub-blog1">
-                      <li className="p-[10px]">Sub-blog 1</li>
-                    </Link>
-                    <Link to="/sub-blog2">
-                      <li className="p-[10px]">Sub-blog 2</li>
-                    </Link>
-                  </ul>
-                </div>
-              )}
-            </li>
-          </Link>
-          <Link to="/media">
-            <li
-              onMouseEnter={() => handleHover("media")}
-              onMouseLeave={handleLeave}
-              className="relative"
-            >
-              Media
-              <span onClick={() => toggleCategory("media")} className="cursor-pointer">
-                {expandedCategory === "media" ? <FaChevronUp /> : <FaChevronDown />}
-              </span>
-              {isSubmenuOpen && hoveredItem === "media" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <ul className="w-[200px] h-[auto] bg-white px-[25px] text-[14px] font-normal text-[#484848]">
-                    <Link to="/media">
-                      <li className="w-[150px] h-[50px] border-b-[2px] flex items-center py-[18px] hover:text-[#2695ff] text-[14px] font-normal">
-                        Media
-                      </li>
-                    </Link>
-                    <Link to="/projects">
-                      <li className="w-[150px] h-[35px] flex items-center py-[18px] hover:text-[#2695ff] text-[14px] font-normal">
-                        Projects
-                      </li>
-                    </Link>
-                  </ul>
-                </div>
-              )}
-              {expandedCategory === "media" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <ul>
-                    <Link to="/sub-media1">
-                      <li className="p-[10px]">Sub-media 1</li>
-                    </Link>
-                    <Link to="/sub-media2">
-                      <li className="p-[10px]">Sub-media 2</li>
-                    </Link>
-                  </ul>
-                </div>
-              )}
-            </li>
-          </Link>
-          <Link to="/about">
-            <li className="relative">
-              About
-              <span onClick={() => toggleCategory("about")} className="cursor-pointer">
-                {expandedCategory === "about" ? <FaChevronUp /> : <FaChevronDown />}
-              </span>
-              {expandedCategory === "about" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <ul>
-                    <Link to="/sub-about1">
-                      <li className="p-[10px]">Sub-about 1</li>
-                    </Link>
-                    <Link to="/sub-about2">
-                      <li className="p-[10px]">Sub-about 2</li>
-                    </Link>
-                  </ul>
-                </div>
-              )}
-            </li>
-          </Link>
-          <Link to="/contact">
-            <li className="relative">
-              Contact
-              <span onClick={() => toggleCategory("contact")} className="cursor-pointer">
-                {expandedCategory === "contact" ? <FaChevronUp /> : <FaChevronDown />}
-              </span>
-              {expandedCategory === "contact" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <ul>
-                    <Link to="/sub-contact1">
-                      <li className="p-[10px]">Sub-contact 1</li>
-                    </Link>
-                    <Link to="/sub-contact2">
-                      <li className="p-[10px]">Sub-contact 2</li>
-                    </Link>
-                  </ul>
-                </div>
-              )}
-            </li>
-          </Link>
-          <Link to="/pages">
-            <li
-              onMouseEnter={() => handleHover("pages")}
-              onMouseLeave={handleLeave}
-              className="relative"
-            >
-              Pages
-              <span onClick={() => toggleCategory("pages")} className="cursor-pointer">
-                {expandedCategory === "pages" ? <FaChevronUp /> : <FaChevronDown />}
-              </span>
-              {isSubmenuOpen && hoveredItem === "pages" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <ul className="w-[200px] h-[auto] bg-white px-[25px] text-[14px] font-normal text-[#484848]">
-                    <Link to="/media">
-                      <li className="w-[150px] h-[50px] border-b-[2px] flex items-center py-[18px] hover:text-[#2695ff] text-[14px] font-normal">
-                        Sitemap
-                      </li>
-                    </Link>
-                    <Link to="/projects">
-                      <li className="w-[150px] h-[35px] flex items-center py-[18px] hover:text-[#2695ff] text-[14px] font-normal">
-                        404 Not Found
-                      </li>
-                    </Link>
-                  </ul>
-                </div>
-              )}
-              {expandedCategory === "pages" && (
-                <div className="absolute z-50 w-[240px] h-[auto] top-12 left-0 bg-white text-black dropdown-menu">
-                  <ul>
-                    <Link to="/sub-pages1">
-                      <li className="p-[10px]">Sub-pages 1</li>
-                    </Link>
-                    <Link to="/sub-pages2">
-                      <li className="p-[10px]">Sub-pages 2</li>
-                    </Link>
-                  </ul>
-                </div>
-              )}
-            </li>
-          </Link>
+      <nav>
+        <ul className="flex gap-[44px] text-white mt-[3%] ml-[8%] text-[14px] font-medium">
+          {data.map((item) => (
+            <Link key={item.id} to={`/${item.concept.toLowerCase()}`}>
+              <li
+                onMouseEnter={() => handleHover(item.id)}
+                onMouseLeave={handleLeave}
+                className={`menu-item relative border-t-2 border-transparent hover:border-white transition duration-300 ${isSubmenuOpen && hoveredItem === item.id ? 'visible' : ''}`}
+                style={{ width: item.id === 1 ? '240px' : 'auto', justifyContent: item.id === 1 ? 'center' : 'initial' }}
+              >
+                {item.icon && <span className="mr-2">{item.icon}</span>}
+                {item.concept}
+                {isSubmenuOpen && hoveredItem === item.id && item.subMenu && (
+                  <div className="absolute z-50 w-[200px] h-auto top-12 left-0 bg-white text-black">
+                    {item.subMenu}
+                  </div>
+                )}
+                {isSubmenuOpen && hoveredItem === item.id && item.concept === "Media" && (
+                  <div className="absolute z-50 w-[200px] h-auto top-12 left-0 bg-white text-black">
+                    <ul className='w-[200px] h-[80px] bg-[white] px-[25px] text-[14px] font-normal text-[#484848]'>
+                      <Link to="/media">
+                        <li className="w-[150px] h-[50px] border-b-[2px] flex items-center py-[18px] hover:text-[#2695ff] text-[14px] font-normal">Media</li>
+                      </Link>
+                      <Link to="/projects">
+                        <li className="w-[150px] h-[35px] flex items-center py-[18px] hover:text-[#2695ff] text-[14px] font-normal">Projects</li>
+                      </Link>
+                    </ul>
+                  </div>
+                )}
+                {isSubmenuOpen && hoveredItem === item.id && item.concept === "Pages" && (
+                  <div className="absolute z-50 w-[200px] h-auto top-12 left-0 bg-white text-black">
+                    <ul className='w-[200px] h-[80px] bg-[white] px-[25px] text-[14px] font-normal text-[#484848]'>
+                      <Link to="/media">
+                        <li className="w-[150px] h-[50px] border-b-[2px] flex items-center py-[18px] hover:text-[#2695ff] text-[14px] font-normal">Sitemap</li>
+                      </Link>
+                      <Link to="/projects">
+                        <li className="w-[150px] h-[35px] flex items-center py-[18px] hover:text-[#2695ff] text-[14px] font-normal">404 Not Found</li>
+                      </Link>
+                    </ul>
+                  </div>
+                )}
+              </li>
+            </Link>
+          ))}
         </ul>
-        <button className="mobile-menu-icon" onClick={toggleMobileMenu}>
-          {isMobileMenuOpen ? <FaTimes /> : <GiHamburgerMenu />}
-        </button>
       </nav>
     </div>
   );
 };
 
-export default NavbarItem;
+export default Navbar;
